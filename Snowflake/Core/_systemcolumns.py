9473635a7f6d5158eb2842216ca36de8433c8366
Ronny Chan <ronny6993@gmail.com>
2013-04-07 22:03:53 -0400

    Lowercase SystemColumns.py

---
+++ Snowflake/Core/_systemcolumns.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+__author__ = 'ron975'
+"""
+This file is part of Snowflake.Core
+"""
+
+GAME_FAQS = "GameFAQs"
+THE_GAMES_DB = "TheGamesDB"
+MOBY_GAMES = "MobyGames"
+SYSTEM_NAME = "SystemName"
+SHORT_NAME = "ShortName"
+GAME_FAQS_URL = "GameFAQs_URL"
+GIANT_BOMB = "GiantBomb"
