2844c8fe6ceb9f571631a06e435130dd1012aa5a
Ronny Chan <ronny6993@gmail.com>
2013-04-10 19:07:26 -0400

    Remove hack for sys.path and snowflakelauncher.py (useless)

--- Snowflake/__main__.py
+++ Snowflake/__main__.py
@@ -3,12 +3,6 @@ __author__ = 'ron975'
 """
 This file is part of Snowflake.Core
 """
-import sys
-try:
-    sys.path.append(sys.argv[1])
-except:
-    pass
-
 import Snowflake.Core.snowflakecore as snowflake_core
 
 
